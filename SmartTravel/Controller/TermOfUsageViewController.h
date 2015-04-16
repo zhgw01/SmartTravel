70b909caf757da78c5b2a4b0b85ab76b9b1dd06a
zhgw01 <zhgw01@gmail.com>
2015-04-16 21:45:29 +0800

    Add image set and term of usage controller

---
+++ SmartTravel/Controller/TermOfUsageViewController.h
@@ -0,0 +1,13 @@
+//
+//  TermOfUsageViewController.h
+//  SmartTravel
+//
+//  Created by Gongwei on 15/4/16.
+//  Copyright (c) 2015年 Gongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TermOfUsageViewController : UIViewController
+
+@end
---
+++ SmartTravel/Controller/TermOfUsageViewController.m
@@ -0,0 +1,18 @@
+//
+//  TermOfUsageViewController.m
+//  SmartTravel
+//
+//  Created by Gongwei on 15/4/16.
+//  Copyright (c) 2015年 Gongwei. All rights reserved.
+//
+
+#import "TermOfUsageViewController.h"
+
+@interface TermOfUsageViewController ()
+
+@end
+
+@implementation TermOfUsageViewController
+
+
+@end
