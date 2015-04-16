4f3554c52c1ce5da6b24b04ae6a652aa9a4a9172
zhgw01 <zhgw01@gmail.com>
2015-04-16 20:13:53 +0800

    Initial Commit

---
+++ SmartTravel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartTravel
+//
+//  Created by Gongwei on 15/4/16.
+//  Copyright (c) 2015年 Gongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartTravel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartTravel
+//
+//  Created by Gongwei on 15/4/16.
+//  Copyright (c) 2015年 Gongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
