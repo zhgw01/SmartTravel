28621a0e337b8fc2fbba5e0bd461a7fc3763fc36
zhgw01 <zhgw01@gmail.com>
2015-04-25 11:17:25 +0800

    Add two view controller

---
+++ SmartTravel/Controller/HotSpotListViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotSpotListViewController.h
+//  SmartTravel
+//
+//  Created by Gongwei on 15/4/25.
+//  Copyright (c) 2015年 Gongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotSpotListViewController : UIViewController
+
+@end
---
+++ SmartTravel/Controller/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  SmartTravel
+//
+//  Created by Gongwei on 15/4/25.
+//  Copyright (c) 2015年 Gongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
