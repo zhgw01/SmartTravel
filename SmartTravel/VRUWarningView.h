aa899e67903f8d11f52fc2e209bdf87bd7f2401d
chenpold <chenpold@shac02kc02afft0.ads.autodesk.com>
2015-05-05 21:16:52 +0800

    Initialize framework of VRU warning view.

---
+++ SmartTravel/VRUWarningView.h
@@ -0,0 +1,13 @@
+//
+//  VRUWarningView.h
+//  SmartTravel
+//
+//  Created by Pengyu Chen on 5/5/15.
+//  Copyright (c) 2015 Gongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VRUWarningView : UIView
+
+@end
---
+++ SmartTravel/VRUWarningView.m
@@ -0,0 +1,67 @@
+//
+//  VRUWarningView.m
+//  SmartTravel
+//
+//  Created by Pengyu Chen on 5/5/15.
+//  Copyright (c) 2015 Gongwei. All rights reserved.
+//
+
+#import "VRUWarningView.h"
+
+@interface VRUWarningView ()
+
+@property (weak, nonatomic) IBOutlet UIView *view;
+@property (weak, nonatomic) IBOutlet UIView *gripView;
+@property (weak, nonatomic) IBOutlet UIView *gripBackgroundView;
+
+@end
+
+@implementation VRUWarningView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    [[NSBundle mainBundle] loadNibNamed:@"VRUWarningView" owner:self options:nil];
+    // Set the view equal width and 1/3 height with parent
+    CGRect myFrame = CGRectMake(frame.origin.x, frame.origin.y, frame.size.width, frame.size.height * 0.3);
+    if (self = [super initWithFrame:myFrame])
+    {
+        [self addSubview:self.view];
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    self.view.frame = self.frame;
+    
+    self.gripView.layer.cornerRadius = 4.0f;
+    self.gripView.layer.masksToBounds = YES;
+}
+
+- (void)didMoveToSuperview
+{
+    UIPanGestureRecognizer* panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    [self.gripBackgroundView addGestureRecognizer:panRecognizer];
+}
+
+- (void)handlePan:(UIPanGestureRecognizer*)recognizer
+{
+    CGPoint translatePoint = [recognizer translationInView:self.gripBackgroundView];
+    // Do not dismiss this view if the pan distance is too small
+    if (translatePoint.y < -5)
+    {
+        [self removeFromSuperview];
+    }
+}
+
+@end
