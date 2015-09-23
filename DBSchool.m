f5942d2f219571611edb6fa84203833636379225
Yuan Huimin <yuanhuimin@YuandeMacBook-Pro.local>
2015-09-23 22:24:15 +0800

    Refactor after upgrade to Xcode7

---
+++ DBSchool.m
@@ -0,0 +1,9 @@
+//
+//  DBSchool.m
+//  SmartTravel
+//
+//  Created by Yuan Huimin on 15/9/23.
+//  Copyright © 2015年 Gongwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
--- SmartTravel/Model/DBConstants.h
+++ SmartTravel/Model/DBConstants.h
@@ -19,3 +19,4 @@ extern NSString * const MAIN_DB_TBL_WM_REASON_CONDITION;
 extern NSString * const MAIN_DB_TBL_LOCATION_REASON;
 extern NSString * const MAIN_DB_TBL_WM_DAYTYPE;
 extern NSString * const MAIN_DB_TBL_NEW_VERSION;
+extern NSString * const MAIN_DB_TBL_SCHOOL;
