01c1c409faaa3c71406f5cff6fb0f04c79447f92
zhanggongwei <zhanggongwei@baidu.com>
2015-10-08 18:52:34 +0800

    add travis ci build script

---
+++ scripts/travis/buildRelease.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xctool -workspace SmartTravel.xcworkspace -scheme SmartTravel -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO 'CODE_SIGN_RESOURCE_RULES_PATH=$(SDKROOT)/ResourceRules.plist'
---
+++ scripts/travis/uploadToPGY.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+IPA_FILE="$PWD/build/Release-iphoneos/$APP_NAME.ipa"
+PGY_UKEY="d53cbab4252ad5baeb6deb540a6fa9fd"
+PGY_APIKEY="18c5a1fa54f90beb4682c1820583549f"
+
+if [ -f $IPA_FILE ]
+then
+   echo "start to upload ipa file: $IPA_FILE"
+   curl -F "file=@$IPA_FILE" -F "uKey=$PGY_UKEY" -F "_api_key=$PGY_APIKEY" -F "publishRange=2" http://www.pgyer.com/apiv1/app/upload
+else
+   echo "$IPA_FILE doesn't exist"
+fi
