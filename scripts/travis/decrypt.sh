29c9a69ffa65dc036ef4106ffba4dcad0e015aee
zhanggongwei <zhanggongwei@baidu.com>
2015-10-08 14:52:41 +0800

    deploy app on travis

---
+++ scripts/travis/decrypt.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/profile/SmartTravelInHouse.mobileprovision.enc -d -a -out scripts/profile/SmartTravelInHouse.mobileprovision
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.cer.enc -d -a -out scripts/certs/dist.cer
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.p12.enc -d -a -out scripts/certs/dist.p12
