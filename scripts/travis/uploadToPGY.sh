f9bb558eda2f7af671fb5a28e30793415c344a8d
zhanggongwei <zhanggongwei@baidu.com>
2015-10-08 16:21:25 +0800

    fix upload script

--- scripts/travis/uploadToPGY.sh
+++ scripts/travis/uploadToPGY.sh
@@ -7,7 +7,7 @@ PGY_APIKEY="18c5a1fa54f90beb4682c1820583549f"
 if [ -f $IPA_FILE ]
 then
    echo "start to upload ipa file: $IPA_FILE"
-   curl -F "file=$IPA_FILE" -F "uKey={$PGY_UKEY}" -F "_api_key={$PGY_APIKEY}" -F "publishRange=2" http://www.pgyer.com/apiv1/app/upload
+   curl -F "file=@$IPA_FILE" -F "uKey=$PGY_UKEY" -F "_api_key=$PGY_APIKEY" -F "publishRange=2" http://www.pgyer.com/apiv1/app/upload
 else
    echo "$IPA_FILE doesn't exist"
 fi
