64f6558067dbc1a86cf6f0bece1979a603c46c1a
zhanggongwei <zhanggongwei@baidu.com>
2015-10-14 11:19:47 +0800

    sign debug app

--- scripts/travis/sign-and-upload.sh
+++ scripts/travis/sign-and-upload.sh
@@ -6,6 +6,7 @@ fi
 
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
+DEBUG_OUTPUTDIR="$PWD/build/Debug-iphoneos"
 
 if [ ! -d "$OUTPUTDIR/$APP_NAME.app" ]; then
    echo "cannot find app"
@@ -13,3 +14,4 @@ if [ ! -d "$OUTPUTDIR/$APP_NAME.app" ]; then
 fi
 
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+xcrun -log -sdk iphoneos PackageApplication "$DEBUG_OUTPUTDIR/$APP_NAME.app" -o "$DEBUG_OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
